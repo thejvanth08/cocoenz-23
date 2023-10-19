201cb3eca5396c7c21c29de78c63e475871d5c40
Thejvanth <thejvanth08@gmail.com>
2023-10-19 17:22:03 +0530

    Upload-2

--- effects.js
+++ effects.js
@@ -1,3 +1,5 @@
+// ----------- Horizontal Scroll
+
 gsap.registerPlugin(ScrollTrigger);
 let sections = gsap.utils.toArray(".slide");
 
@@ -6,19 +8,174 @@ gsap.to(sections, {
   ease: "none",
   scrollTrigger: {
     trigger: ".horizontal-sliders",
-    pin: ".body",
+    // pin: ".body",
+    pin: true,
     pinSpacing: true,
     scrub: 1,
     end: "+=1500",
   },
 });
 
-gsap.to(".next-block", {
-  // backgroundColor: "tomato",
-  scrollTrigger: {
-    trigger: ".next-block",
-    pinnedContainer: ".body",
-    start: "top 50%",
-    toggleActions: "play none reset none",
+// gsap.to(".next-block", {
+//   // backgroundColor: "tomato",
+//   scrollTrigger: {
+//     trigger: ".next-block",
+//     pinnedContainer: ".body",
+//     start: "top 50%",
+//     toggleActions: "play none reset none",
+//   },
+// });
+
+// ------------------------- For particles enable
+const config = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#fcee0a",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#fffccc",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
   },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: false,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS("particles-js", config);
+});
+
+// ------------------- AOS
+const aosConfig = {
+  duration: 1000,
+};
+
+AOS.init(aosConfig);
+
+// --------------------- About Card hover effect
+
+document.getElementById("cards").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
+
+//------------------------ Slider effects
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 1,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
 });
--- script.js
+++ script.js
@@ -1,128 +1,34 @@
-// ------------------------- For particles enable
-const config = {
-  particles: {
-    number: {
-      value: 100,
-      density: {
-        enable: true,
-        value_area: 800,
-      },
-    },
-    color: {
-      value: "#fcee0a",
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000",
-      },
-      polygon: {
-        nb_sides: 5,
-      },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100,
-      },
-    },
-    opacity: {
-      value: 0.8,
-      random: true,
-      anim: {
-        enable: true,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
-      },
-    },
-    size: {
-      value: 3,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: true,
-      distance: 100,
-      color: "#ffffff",
-      opacity: 0.6,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 3,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-      bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200,
-      },
-    },
-  },
-  interactivity: {
-    detect_on: "window",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "repulse",
-      },
-      onclick: {
-        enable: true,
-        mode: "bubble",
-      },
-      resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 400,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 400,
-        size: 4,
-        duration: 2,
-        opacity: 8,
-        speed: 3,
-      },
-      repulse: {
-        distance: 100,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
-      },
-    },
-  },
-  retina_detect: true,
-};
-
 document.addEventListener("DOMContentLoaded", function () {
-  particlesJS("particles-js", config);
-});
+  // Get a reference to the .menu element
+  const menu = document.querySelector(".menu");
 
-// Card hover effect
+  // Get references to the events, schedule, and about sections
+  const eventsSection = document.getElementById("events");
+  const scheduleSection = document.getElementById("schedule");
+  const aboutSection = document.getElementById("about");
 
-document.getElementById("cards").onmousemove = (e) => {
-  for (const card of document.getElementsByClassName("card")) {
-    const rect = card.getBoundingClientRect(),
-      x = e.clientX - rect.left,
-      y = e.clientY - rect.top;
-
-    card.style.setProperty("--mouse-x", `${x}px`);
-    card.style.setProperty("--mouse-y", `${y}px`);
+  // Function to check if the scroll position is within a specific section
+  function isScrollInSection(section) {
+    const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    const sectionOffset = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    return (
+      scrollPosition >= sectionOffset &&
+      scrollPosition < sectionOffset + sectionHeight
+    );
   }
-};
+
+  // Add a scroll event listener to the window
+  window.addEventListener("scroll", function () {
+    if (isScrollInSection(eventsSection)) {
+      // If it is, add the .menu-hidden class to hide the menu
+      menu.classList.add("menu-hidden");
+    } else if (
+      isScrollInSection(scheduleSection) ||
+      isScrollInSection(aboutSection)
+    ) {
+      // If it is within the schedule or about section, remove the .menu-hidden class to show the menu
+      menu.classList.remove("menu-hidden");
+    }
+  });
+});
