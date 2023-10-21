03da56668233c0603a73c94432d9cf4f3b385beb
Thejvanth <thejvanth08@gmail.com>
2023-10-21 15:33:22 +0530

    fix-5

--- script.js
+++ script.js
@@ -77,9 +77,257 @@ const scrollHeader = () => {
   // when window is scrolled vertically i.e greater than 50,
   // add the scroll class to the header tag
   // console.log(this); this refers window object which is calling the function as a cb
-  this.scrollY >= 50
+  this.scrollY >= 100
     ? header.classList.add("bg-header")
     : header.classList.remove("bg-header");
 };
 
 window.addEventListener("scroll", scrollHeader);
+
+// ----------- To automatically close the navbar
+const menuItems = document.querySelectorAll(".menu__item");
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", () => {
+    const checkbox = document.getElementById("main-navigation-toggle");
+    checkbox.checked = false;
+  });
+});
+
+// ********************* Effects ******************
+// ----------- Horizontal Scroll
+
+gsap.registerPlugin(ScrollTrigger);
+let sections = gsap.utils.toArray(".slide");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".horizontal-sliders",
+    // pin: ".body",
+    pin: true,
+    pinSpacing: true,
+    scrub: 1,
+    end: "+=1500",
+  },
+});
+
+// ------------------------- For particles enable
+const config = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#fcee0a",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#fffccc",
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 3,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: true,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 6,
+        duration: 2,
+        opacity: 5,
+        speed: 3,
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.3,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
+};
+
+// For about section
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS("particles-js", config);
+});
+
+// For team section
+document.addEventListener("DOMContentLoaded", function () {
+  particlesJS("particles-two-js", config);
+});
+
+// ------------------- AOS
+const aosConfig = {
+  duration: 1000,
+};
+
+AOS.init(aosConfig);
+
+// --------------------- About Card hover effect
+
+document.getElementById("cards").onmousemove = (e) => {
+  for (const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  }
+};
+
+//------------------------ Slider effects
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 1,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 3000, // Set the delay between slides in milliseconds (e.g., 3000ms or 3 seconds)
+    disableOnInteraction: false, // Set this to true if you want autoplay to stop when the user interacts with the slider (e.g., clicks or swipes)
+  },
+  loop: true, // Add this line to enable the loop feature
+});
+
+// Shuffle effect
+document.addEventListener("DOMContentLoaded", function () {
+  // Set effect velocity in ms
+  // high means slow
+  var velocity = 80;
+
+  var shuffleElements = document.querySelectorAll(".shuffle");
+
+  shuffleElements.forEach(function (element) {
+    element.dataset.text = element.textContent;
+  });
+
+  var shuffle = function (o) {
+    for (
+      var j, x, i = o.length;
+      i;
+      j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+    );
+    return o;
+  };
+
+  var shuffleText = function (element, originalText) {
+    var elementTextArray = [];
+    var randomText = [];
+
+    for (var i = 0; i < originalText.length; i++) {
+      elementTextArray.push(originalText.charAt(i));
+    }
+
+    var repeatShuffle = function (times, index) {
+      if (index == times) {
+        element.textContent = originalText;
+        return;
+      }
+
+      setTimeout(function () {
+        randomText = shuffle(elementTextArray);
+        for (var i = 0; i < index; i++) {
+          randomText[i] = originalText[i];
+        }
+        randomText = randomText.join("");
+        element.textContent = randomText;
+        index++;
+        repeatShuffle(times, index);
+      }, velocity);
+    };
+
+    repeatShuffle(element.textContent.length, 0);
+  };
+
+  shuffleElements.forEach(function (element) {
+    element.addEventListener("mouseenter", function () {
+      shuffleText(this, this.dataset.text);
+    });
+  });
+});
