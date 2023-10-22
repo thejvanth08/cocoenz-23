8badb94a51e2354233ce85bb39573a1422a547f8
Thejvanth <thejvanth08@gmail.com>
2023-10-22 12:43:56 +0530

    Fix footer-map change-logo confetti-rules

---
+++ event-rules/script.js
@@ -0,0 +1,50 @@
+const register = document.getElementById("register");
+
+register.addEventListener("click", () => {
+  const count = 200;
+  const defaults = {
+    origin: { y: 0.7 },
+  };
+
+  function fire(particleRatio, opts) {
+    confetti(
+      Object.assign({}, defaults, opts, {
+        particleCount: Math.floor(count * particleRatio),
+        shapes: ["star"],
+        colors: ["#fcee0a", "#00f0ff"], // Set the color of the particles to white (stars)
+      })
+    );
+  }
+
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+
+  fire(0.2, {
+    spread: 60,
+  });
+
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+
+  setTimeout(() => {
+    // Redirect to another page after 1 second
+    console.log("congrats");
+  }, 1500);
+});
