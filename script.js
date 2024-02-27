2508387af9e26fb8c4d531978dea38eb7e9e2dfc
Thejvanth <thejvanth08@gmail.com>
2024-02-27 19:41:22 +0530

    event over

--- script.js
+++ script.js
@@ -101,35 +101,35 @@ const minutesElement = countdownElement.querySelector("#minutes");
 const secondsElement = countdownElement.querySelector("#seconds");
 
 // EVENT DATE -> 27/2/24
-const endDate = new Date("2024-2-27 00:00:00");
-let secondsLeft = (endDate.getTime() - new Date().getTime()) / 1000;
+// const endDate = new Date("2024-2-27 00:00:00");
+// let secondsLeft = (endDate.getTime() - new Date().getTime()) / 1000;
 
-function updateCountdown() {
-  secondsLeft -= 1;
+// function updateCountdown() {
+//   secondsLeft -= 1;
 
-  const days = Math.floor((secondsLeft / 86400) % 30);
-  daysElement.textContent = days < 10 ? "0" + days : days;
+//   const days = Math.floor((secondsLeft / 86400) % 30);
+//   daysElement.textContent = days < 10 ? "0" + days : days;
 
-  const hours = Math.floor((secondsLeft % 86400) / 3600);
-  hoursElement.textContent = hours < 10 ? "0" + hours : hours;
+//   const hours = Math.floor((secondsLeft % 86400) / 3600);
+//   hoursElement.textContent = hours < 10 ? "0" + hours : hours;
 
-  const minutes = Math.floor((secondsLeft % 3600) / 60);
-  minutesElement.textContent = minutes < 10 ? "0" + minutes : minutes;
+//   const minutes = Math.floor((secondsLeft % 3600) / 60);
+//   minutesElement.textContent = minutes < 10 ? "0" + minutes : minutes;
 
-  const seconds = Math.floor(secondsLeft % 60);
-  secondsElement.textContent = seconds < 10 ? "0" + seconds : seconds;
+//   const seconds = Math.floor(secondsLeft % 60);
+//   secondsElement.textContent = seconds < 10 ? "0" + seconds : seconds;
 
-  if (secondsLeft <= 0) {
-    const countdownContent = document.querySelector(".countdown-content");
-    const countdownContentFinal = document.querySelector(
-      ".countdown-content-final"
-    );
-    countdownContent.classList.add("d-none");
-    countdownContentFinal.classList.add("d-flex");
-  }
-}
+//   if (secondsLeft <= 0) {
+//     const countdownContent = document.querySelector(".countdown-content");
+//     const countdownContentFinal = document.querySelector(
+//       ".countdown-content-final"
+//     );
+//     countdownContent.classList.add("d-none");
+//     countdownContentFinal.classList.add("d-flex");
+//   }
+// }
 
-setInterval(updateCountdown, 1000);
+// setInterval(updateCountdown, 1000);
 
 // --------- To add bg when above some scroll position
 const scrollHeader = () => {
